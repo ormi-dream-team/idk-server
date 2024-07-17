1f17096762ea9d1ac6626a7b9c1f884763c34cb8
jsween5723 <jsween5723@gmail.com>
2024-07-17 23:20:55 +0900

    build(init): 프로젝트 초기화

---
+++ src/main/java/team/onepoom/idkserver/IdkServerApplication.java
@@ -0,0 +1,13 @@
+package team.onepoom.idkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdkServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IdkServerApplication.class, args);
+  }
+
+}
