4722fd205d47b255c51d15364d55101ed0ace939
jsween5723 <jsween5723@gmail.com>
2024-07-23 07:50:15 +0900

    build(setting): 기타 설정변경

--- src/main/java/team/onepoom/idkserver/IdkServerApplication.java
+++ src/main/java/team/onepoom/idkserver/IdkServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IdkServerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(IdkServerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(IdkServerApplication.class, args);
+    }
 
 }
