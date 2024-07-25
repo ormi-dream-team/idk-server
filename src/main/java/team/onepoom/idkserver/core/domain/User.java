92650aa1e74904d56205910de95b94916607b6c0
hyezuu <duudu0704@gmail.com>
2024-07-25 13:25:22 +0900

    feat(user): User entity update

--- src/main/java/team/onepoom/idkserver/core/domain/User.java
+++ src/main/java/team/onepoom/idkserver/core/domain/User.java
@@ -5,16 +5,31 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import team.onepoom.idkserver.core.domain.common.BaseEntity;
 
+@Getter
 @Entity
 @Table(name = "users")
-class User extends BaseEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
     @Column(nullable = false)
     private String email;
     @Column(nullable = false, length = 255)
     private String password;
+    @OneToMany(mappedBy = "user")
+    @Column(nullable = false)
+    private List<UserRole> roles;
+
+    public User(Long id) {
+        this.id = id;
+    }
+
 }
