2b69d388dd5c0c297ca7325016d6f5c6c2de939e
hyezuu <duudu0704@gmail.com>
2024-07-25 13:26:40 +0900

    feat(exception): ConflictException create

---
+++ src/main/java/team/onepoom/idkserver/core/domain/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package team.onepoom.idkserver.core.domain.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
